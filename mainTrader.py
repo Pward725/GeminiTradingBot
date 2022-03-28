ec395c87fba322e9bba932abd0a84ffaae35ede4
pward725 <phillip.j.ward725@gmail.com>
2022-03-28 14:32:49 -0600

    added gets for price and ticker info

--- mainTrader.py
+++ mainTrader.py
@@ -1,6 +1,15 @@
-
+# Docs: https://docs.gemini.com/rest-api/v2
+import json, requests
+import pandas as pd
 
 def run():
+    base_url = "https://api.sandbox.gemini.com"
+    response = requests.get(base_url + "/symbols")
+    symbols = pd.DataFrame(response.json())
+    symbols.tail()
+
+    response = requests.get(base_url + "/ticker/btcusd")
+    btc_data = response.json()
     #get some price data
     #plug that into model
      
